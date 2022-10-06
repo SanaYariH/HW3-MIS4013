4672944b9bbde5d48db1fc8a1ac2e4adadd3d542
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-10-06 12:57:31 -0500

    Update sections.php

--- sections.php
+++ sections.php
@@ -21,9 +21,9 @@
   <tbody>
     <?php
 $servername = "localhost";
-$username = "projecto_homework3";
-$password = "0w_zeP}]OVy0";
-$dbname = "projecto_homework3";
+$username = "sanaoucr_syuser";
+$password = "zxvf=zsw;*iY";
+$dbname = "sanaoucr_homework3-MIS4013";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
