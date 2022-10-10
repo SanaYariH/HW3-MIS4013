735adc704fccabb14b992ebd6595dab64ad8a02e
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-10-10 12:33:39 -0500

    Update jobs.php

--- jobs.php
+++ jobs.php
@@ -1,20 +1,16 @@
-<!doctype html>
-<html lang="en">
-  <head>
+<?php require_once('header.php');?>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Instructors</title>
+    <title>Student Jobs</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Student Jobs</h1>
 <table class="table table-striped">
   <thead>
     <tr>
-      <th>ID</th>
-      <th>Job Title</th>
-      <th></th>
-      <th></th>
+      <th>Job ID</th>
+      <th>Job Titles</th>
     </tr>
   </thead>
   <tbody>
@@ -39,14 +35,8 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
 ?>
   <tr>
-    
-    <td>
-      
-    </td>
-    <td>
-      
-    
-    </td>
+    <td><?=$row["job_id"]?></td>
+    <td><?=$row["job_title"]?></td>
   </tr>
 <?php
   }
@@ -57,8 +47,14 @@ $conn->close();
 ?>
   </tbody>
     </table>
-    <br />
+    <h1>You wanna find jobs?!!</h1>
+    <form action="handlepost.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
     
+    <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
