3788654959815d8b44a624554d198b4445cb63cb
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-10-10 12:35:52 -0500

    Update jobs.php

--- jobs.php
+++ jobs.php
@@ -1,4 +1,4 @@
-
+    <h1>You wanna find jobs?!!</h1>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -47,7 +47,7 @@ $conn->close();
 ?>
   </tbody>
     </table>
-    <h1>You wanna find jobs?!!</h1>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
