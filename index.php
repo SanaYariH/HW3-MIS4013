059bd43547e88b5d071aca7d153c0c57abd261ed
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-10-10 12:40:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php require_once('header.php');?>
 
+<h1>Sana Yari's HomeWork 3!</h1>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sana's HomeWork 3</title>
@@ -47,12 +49,8 @@ $conn->close();
 ?>
   </tbody>
     </table>
-    <h1>Sana Yari's HomeWork 3!</h1>
-    <form action="handlepost.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
+    
+
     
     <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
